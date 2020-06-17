c37727af0a81e37404f540e13c0250c31c8691ff
xDaile <mychalze@gmail.com>
2020-06-17 21:32:49 +0200

    net is able to train

--- baseCNN.py
+++ baseCNN.py
@@ -1,14 +1,47 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import os
-import numpy as np
+import numpy
 import getFiles
 import parameters
 import torch
 import torchfile
 import cv2
 import Model
+import sys
+from notify_run import Notify
+from sklearn.metrics import confusion_matrix
 
+notify = Notify()
+
+
+def accuracy(prediction, truth):
+    """ Returns the confusion matrix for the values in the `prediction` and `truth`
+    tensors, i.e. the amount of positions where the values of `prediction`
+    and `truth` are
+    - 1 and 1 (True Positive)
+    - 1 and 0 (False Positive)
+    - 0 and 0 (True Negative)
+    - 0 and 1 (False Negative)
+    """
+
+    confusion_vector = prediction / truth
+    print(confusion_vector)
+    # Element-wise division of the 2 tensors returns a new tensor which holds a
+    # unique value for each case:
+    #   1     where prediction and truth are 1 (True Positive)
+    #   inf   where prediction is 1 and truth is 0 (False Positive)
+    #   nan   where prediction and truth are 0 (True Negative)
+    #   0     where prediction is 0 and truth is 1 (False Negative)
+
+    true_positives = torch.sum(confusion_vector == 1).item()
+    false_positives = torch.sum(confusion_vector == float('inf')).item()
+    true_negatives = torch.sum(torch.isnan(confusion_vector)).item()
+    false_negatives = torch.sum(confusion_vector == 0).item()
+    precision=true_positives/(true_positives+false_positives)
+    recall=true_positives/(true_positives+false_negatives)
+    maxF=2*((precision*recall)/(precision+recall))
+    return maxF
 
 class Dataset(torch.utils.data.Dataset):
   'Characterizes a dataset for PyTorch'
@@ -30,6 +63,8 @@ class Dataset(torch.utils.data.Dataset):
         # Load data and get label
         X = torch.load(self.tensorDict[key])#HERE I ENDED
         y = torch.load(self.GTDict[key])
+        X=X.to(device)
+        y=y.to(device)
         return X, y
 
 
@@ -45,12 +80,13 @@ def get_device():
         print("Device was not changed to gtx 960m")
         device = torch.device('cpu') # don't have GPU
 
+
 get_device()
 tensors=getFiles.loadListOfTensors()
 groundTruth=getFiles.getListOfGroundTruthFiles()
 listIDs=getFiles.getListOfIDs()
 
-
+from notify_run import Notify
 params = {"train":{
             'shuffle': True,
             #'batch_size': 64,
@@ -67,30 +103,71 @@ validation_set = Dataset(listIDs['test'],tensors['test'],groundTruth['test'])
 validation_generator = torch.utils.data.DataLoader(training_set, **params['test'])
 
 model= Model.Net()
-loss = torch.nn.CrossEntropyLoss(ignore_index=3)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+if(os.path.exists("./model.pth")):
+    print("Model will be loaded from saved state")
+    model.load_state_dict(torch.load("./model.pth"))
+    model.eval()
+else:
+    print("model not found, starting from scratch")
 
-#print(model)
 
-stopTraining=True
-#loop while not overfitted
+criterion = torch.nn.CrossEntropyLoss(ignore_index=3)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+view_step=10
+continueTraining=True
 loss_acc=0
-#model.eval()
+accuracy_acc=0
+iteration=0
 model.train()
-while(stopTraining):
-    for inputForNetwork,outputFromNetwork in training_generator:
 
-        inputForNetwork.to(device)
-        outputFromNetwork.to(device)
-#        print(inputForNetwork)
-        print(outputFromNetwork)
+def test(model, data_loader):
+    model=model.eval()
+    loss_acc=0
+    accuracy_acc=0
+    counter=0
+    for inputForNetwork,outputFromNetwork in data_loader:
         result=model(inputForNetwork)
-    #    torch.autograd.set_detect_anomaly(True)
-        l = loss(result,outputFromNetwork)
-        optimizer.zero_grad()
-        l.backward()
-        optimizer.step()
-        loss_acc+=loss.item()
-        print(loss_acc)
-        stopTraining=False
-        break
+        loss=criterion(result,outputFromNetwork)
+        loss_acc=loss_acc+loss.item()
+        accuracy=accuracy(outputFromNetwork,result)
+        accuracy_acc=accuracy_acc+accuracy
+        counter+=1
+        #break
+    model=model.train()
+    return loss_acc/counter , accuracy_acc/counter
+
+
+i=0
+while(continueTraining):
+
+    lenghtOfTrainingInputs=len(training_generator)
+    model.to(device)
+    for inputForNetwork,outputFromNetwork in training_generator:
+        result=model(inputForNetwork)
+        loss = criterion(result,outputFromNetwork)
+        optimizer.zero_grad()#see doc
+        loss.backward()#see doc
+        optimizer.step()#see doc
+        loss_acc=loss_acc+loss.item()
+        accuracy=accuracy(outputFromNetwork,result)
+        accuracy_acc=accuracy_acc+accuracy
+        #break
+
+    if(i%view_step==0):
+        test_loss_acc, test_accuracy_acc=test(model,validation_generator)
+        message="Iteration:" + str(i) + "\nLoss:" + str(loss_acc/view_step) + "\nAccuracy:" + str(accuracy_acc/view_step) + "\nTestLoss:" + str(test_loss_acc) + "\nTestAccuracy_acc:" + str(test_accuracy_acc)
+        loss_acc=0
+        accuracy_acc=0
+        test_loss_acc=0
+        test_accuracy_acc=0
+        try:
+            notify.send(message)
+        except:
+            print("failed to send")
+
+        if(os.path.exists("./stop")):
+                print("saving model params")
+                model.eval()
+                torch.save(model.state_dict(), "./model.pth")
+                continueTraining=False
+    i=i+1
