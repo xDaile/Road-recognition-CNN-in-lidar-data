3f434fecf4f9c27e14b16d0f806dcf60291e42cb
xDaile <mychalze@gmail.com>
2020-06-17 21:35:03 +0200

    added some files, there do not need to be on github but why not

---
+++ CMakeFiles/pclviewer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pclviewer.dir/pclviewer.cpp.o"
+  "pclviewer.pdb"
+  "pclviewer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pclviewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/pclwiever.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pclwiever.dir/pclviewer.cpp.o"
+  "pclwiever.pdb"
+  "pclwiever"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pclwiever.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
