290827bb44981dbd185ba01bda1a935849e56ab9
xDaile <mychalze@gmail.com>
2020-06-24 10:46:17 +0200

    changed LR

--- baseCNN.py
+++ baseCNN.py
@@ -111,7 +111,7 @@ continueTraining=True
 loss_sum=0
 accuracy_sum=0
 iteration=0
-learning_rate=0.0001
+learning_rate=0.001
 
 #model needs to be created too if it will be loaded
 model= Model.Net()
