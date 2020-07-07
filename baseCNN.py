bf5f5c083feb80fe5428f1e89bed416b63b458dc
xDaile <mychalze@gmail.com>
2020-07-07 19:37:18 +0200

    calculation some more stats

--- baseCNN.py
+++ baseCNN.py
@@ -266,9 +266,9 @@ while(continueTraining):
                     + "\tTestAccuracy:" + "{:.2f}".format(test_accuracy)                        \
                     + "\ttestVariation:"+ "{:.2f}".format(test_variation)                       \
                     + "\tTestMaxF:"+"{:.2f}".format(test_maxF)                                  \
-                    + "\tTestMaxFPrecise:"+"{:.2f}".format(test_maxF_precise)                   \   #in images was not class 2 should be something like maxf
-                    + "\tTestACCPrecise:"+"{:.2f}".format(test_acc_precise)                     \   #same as line above
-                    + "\tACCPrecise:"+"{:.2f}".format(maxF_Precise/withoutACCmiss)              \   
+                    + "\tTestMaxFPrecise:"+"{:.2f}".format(test_maxF_precise)                   \   
+                    + "\tTestACCPrecise:"+"{:.2f}".format(test_acc_precise)                     \
+                    + "\tACCPrecise:"+"{:.2f}".format(maxF_Precise/withoutACCmiss)              \
                     + "\tMaxPrecise:"+"{:.2f}".format(acc_Precise/withoutACCmiss)               \
 
 
